38a5c1cb6310aa3775f19dc9bad6e59e8337e3d4
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-04-12 14:51:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     time = Time.now
     resp = Rack::Response.new
 
-    if Time < 12
+    if time < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
