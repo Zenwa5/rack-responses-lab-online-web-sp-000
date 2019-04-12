a0464ba5f3a3af9fc32f09d108b960b1b21c3414
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-04-12 14:49:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,5 +2,10 @@ class Application
   Time.new
   def call(env)
     resp = Rack::Response.new
+
+    if Time < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
   end
 end
